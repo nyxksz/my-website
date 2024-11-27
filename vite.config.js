217148562227e0c17f4d09e77efad0284ad52a45
nyxksz <144878170+nyxksz@users.noreply.github.com>
2024-11-28 02:50:49 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  server: {
+    host: '0.0.0.0', // Expose to external networks
+    port: process.env.PORT || 5173, // Use Render's assigned port or default to 5173
+  },
+});
+
